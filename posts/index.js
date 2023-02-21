cc3e3ba3444f818fc1a5a00eeb7f20d4b935dc8c
Dhandapani R <rdhandapani26@gmail.com>
2023-02-21 19:34:04 +0530

    project setup and added posts fuction tested in postman

---
+++ posts/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const req = require('express/lib/request');
+const res = require('express/lib/response');
+const {randomBytes} = require('crypto');
+
+const app = express();
+app.use(bodyParser.json());
+
+const posts = {};
+
+app.get('/posts', (req, res) => {
+    res.send(posts); // get all the posts list
+});
+app.post('/posts', (req, res) => {
+    const id = randomBytes(4).toString('hex');
+    const {title} = req.body;
+
+    posts[id] = {
+        id, title
+    };
+    res.status(201).send(posts[id]); //return post with title
+});
+
+app.listen('4000', () => {
+console.log('listening on 4000');
+});
