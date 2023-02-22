2d75c1a91dc3eef2ac1b71364b9b8f972ebc1dd8
Dhandapani R <rdhandapani26@gmail.com>
2023-02-22 13:43:59 +0530

    post list are retrived and displayed

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,15 @@
 import React from "react";
 import PostCreate from "./PostCreate";
+import PostList from "./PostList";
 
-export default ()  => {
+export default () => {
     return <div className="container">
         <h1> Client Post</h1>
         <PostCreate />
+        <hr />
+        <div>
+        <h1>Post Lists</h1>
+        <PostList />
+        </div>
     </div>
 };
