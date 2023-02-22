411a5d605646da80582092ab4e53b78cc03c2dea
Dhandapani R <rdhandapani26@gmail.com>
2023-02-22 20:02:44 +0530

    emitting post creation events, comment creation events and receiving events

--- posts/index.js
+++ posts/index.js
@@ -4,6 +4,7 @@ const req = require('express/lib/request');
 const res = require('express/lib/response');
 const {randomBytes} = require('crypto');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
@@ -13,14 +14,25 @@ const posts = {};
 app.get('/posts', (req, res) => {
     res.send(posts); // get all the posts list
 });
-app.post('/posts', (req, res) => {
+app.post('/posts', async (req, res) => {
     const id = randomBytes(4).toString('hex');
     const {title} = req.body;
 
     posts[id] = {
         id, title
     };
+
+    await axios.post('http://localhost:4005/events',{
+        type : 'PostCreated',
+        data :{ id, title }
+    });
+
     res.status(201).send(posts[id]); //return post with title
+});
+
+app.post('/events', (req, res) => {
+    console.log('Received Event: ', req.body.type);
+    res.status({});
 });
 
 app.listen('4000', () => {
